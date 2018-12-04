76eccb988eff70a356bbccd7796797286059f976
hashmap27@gmail.com <hashmap27@gmail.com>
2018-12-04 11:17:05 +0900

    add spring starter dependecies

---
+++ src/main/java/com/sample/myapplication/BootStarterSampleApplication.java
@@ -0,0 +1,12 @@
+package com.sample.myapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootStarterSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootStarterSampleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sample/myapplication/BootStarterSampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sample.myapplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootStarterSampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
